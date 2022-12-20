3557e4b908629c6606dc45a792e0ca44b2459f5b
EBrahim Muhammed <e.muuhammed@gmail.com>
2022-12-20 21:19:01 +0200

    initial commit
    first

---
+++ KNN.py
@@ -0,0 +1,117 @@
+
+
+# In[134]:
+
+
+import pandas as pd
+import numpy as np
+import math
+import random
+
+data = pd.read_csv('BankNote_Authentication.csv')
+data = np.array(data)
+#np.random.shuffle(data)
+
+m = 960
+# In[135]:
+
+
+mean_list = []
+for i in range(m):
+    mean=0.0
+    for j in range(4):
+        mean = mean + data[i][j]
+    mean=mean/m
+    mean_list.append(mean)
+
+
+# In[136]:
+
+
+std_list = []
+for i in range(m):
+    std=0.0
+    for j in range(4):
+        std= std+ ((data[i][j]-mean_list[j])*(data[i][j]-mean_list[j]))
+    std=std/m
+    std= math.sqrt(std)
+    std_list.append(std)
+
+
+# In[137]:
+
+
+size = len(data)
+for i in range(m):
+    for j in range(4):
+        data[i][j] = ((data[i][j]-mean_list[j])/std_list[j])
+
+data_train = data[:960]
+data_test = data[960:]        
+
+
+# In[138]:
+
+
+def knn (x_list,k):
+    min_dist = []
+    for i in range (m):
+        distance = 0.0
+        for j in range (4):
+            distance += ((x_list[j]-data_train[i][j]) * (x_list[j]-data_train[i][j]))
+        distance = math.sqrt(distance)
+        list_index =[distance,i]
+        min_dist.append(list_index)   
+    min_dist.sort()
+    #print(min_dist)
+
+    zeroes=0
+    ones =0
+    i = min_dist[0]
+    val = data_train[i[1]][4]
+    for i in range (k):
+        ind = min_dist[i]
+        last_index = ind[1]
+        if data_train[last_index][4] == 0.0:
+            zeroes+=1
+        else:
+            ones +=1 
+    if zeroes>ones :
+        return 0.0
+    elif ones>zeroes:
+        return 1.0 
+    else :
+        return val
+
+
+# In[139]:
+
+
+test_size= len(data_test)
+def run (k):
+    acc=0.0
+    for i in range(test_size):
+        row = [data_test[i][0],data_test[i][1],data_test[i][2],data_test[i][3]]
+        r= knn(row,k)
+       # print(r)
+        if  r == data_test[i][4]:
+            acc +=1
+    acc = acc
+    return acc
+
+
+# In[140]:
+
+
+for i in range(1,12):
+    print("k value:",i)
+    acc = run(i)
+    print("Number of correctly classified instances :", acc, " Total number of instances :" ,test_size)
+    print("Accuracy : ",(acc/test_size))
+
+
+# In[ ]:
+
+
+
+
